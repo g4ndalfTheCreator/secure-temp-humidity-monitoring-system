41bd353609d4007160e1e39cd71e4958240aaf0d
g4ndalfTheCreator <zampee01@gmail.com>
2024-04-10 14:21:33 +0300

    working basic mqtt msg broker

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -1,94 +1,114 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <DFRobot_DHT11.h>
 
-const char* ssid = "WIFINAME";
-const char* password = "WIFI_PASS";
+#define seconds() (millis()/1000)
 
-const char* mqtt_server = "SERVER_IP";
-const char* mqttUser = "user";
-const char* mqttPassword = "pass";
+// Set up temp sensors DHT11
+DFRobot_DHT11 DHT;
+#define DHT11_PIN D3
 
+// Define wifi connection
+const char *ssid = "x";
+const char *password = "x"; 
+
+// Define mqtt connection
+const char *mqtt_broker = "192.168.0.112"; 
+const char *user = "test"; // not real username
+const char *pw = "pw"; // not real password
+
+// Define topics
+const char *temp_topic = "temperature";
+const char *hum_topic = "humidity";
+
+// define hum/temp
+float temperature;
+float humidity;
+
+// Define times
+unsigned long prev_seconds = 0; 
+unsigned refresh_rate = 10; // sec
+
+const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
 
-void setup_wifi() {
+// Adds values into arraylogs
+void update_values(){
+  
+  // Read temperature and humidity
+  DHT.read(DHT11_PIN);
+  temperature = DHT.temperature;
+  humidity = DHT.humidity;
 
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str(), mqttUser, mqttPassword)) {
-      Serial.println("connected");
-      client.subscribe("CoreElectronics/test");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+void setup_connections(){
+    // connecting to a WiFi network
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi network: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
-  }
+    Serial.print("\nConnected with ip: ");
+    Serial.println(WiFi.localIP());
+
+        //connecting to a mqtt broker
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    
+    while (!client.connected()) {
+        String client_id = "esp8266-client-";
+        client_id += String(WiFi.macAddress());
+    
+        Serial.printf("The client %s connects to mosquitto mqtt broker\n", client_id.c_str());
+ 
+        if (client.connect(client_id.c_str(), user, pw)) {
+            Serial.println("Public emqx mqtt broker connected");
+        } 
+        else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+    Serial.begin(9600);
+    
+    setup_connections();
+    // publish and subscribe
+    client.publish(temp_topic, "Hello From ESP8266!");
+    //client.subscribe(temp_topic);
+}
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    
+    for (int i = 0; i < length; i++) {
+    Serial.print((char) payload[i]);
+    }
+    
+    Serial.println();
+    Serial.println(" - - - - - - - - - - - -");
 }
 
 void loop() {
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+    unsigned long seconds = seconds();
+
+    if(seconds - prev_seconds >= refresh_rate){
+        update_values();
+        client.publish(temp_topic, String(temperature).c_str());
+        client.publish(hum_topic, String(humidity).c_str());
+        prev_seconds = seconds();
+    }
+
+    client.loop();
+
+    
 }
